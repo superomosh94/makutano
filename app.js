735a9c195fe5879db3c36ef66f0235d7b8a1a230
superomosh94 <omondo94@gmail.com>
2025-08-25 11:56:36 +0300

    ni

--- app.js
+++ app.js
@@ -1,33 +1,82 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
-  database: "socialapp",
-  port: 3307,
+  password: "Roguomondo1",
+  database: "socialapp2",
+  port: 3306,
 });
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/users", (req, res) => {
-  console.log(req.query.id);
-  console.log(req.query.person);
-  connection.query("SELECT * FROM users", (dberr, results) => {
-    if (dberr) {
-      return res.status(500).send("Error retrieving users" + dberr);
+// add new post to posts in the database
+app.post("/newposts", (req, res) => {
+  const { content } = req.body;
+  const query = "INSERT INTO posts (content, postowner) VALUES (?, ?)";
+  connection.query(query, [content, 1], (err, result) => {
+    if (err) {
+      return res.status(500).send("Error adding post: " + err);
     }
-    res.render("users.ejs", { users: results });
+    res.redirect("/posts"); // redirect once post is added
   });
 });
-app.get("/posts", (req, res) => {});
+
+// show posts page
+app.get("/posts", (req, res) => {
+  const query = `
+    SELECT posts.postid, posts.content, posts.createdat, users.fullname
+    FROM posts
+    JOIN users ON posts.postowner = users.userid
+    ORDER BY posts.createdat DESC
+  `;
+
+  connection.query(query, (err, posts) => {
+    if (err) {
+      return res.status(500).send("Error retrieving posts: " + err);
+    }
+    res.render("posts.ejs", { posts: posts });
+  });
+});
+
+// show users page
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM users", (err, users) => {
+    if (err) {
+      return res.status(500).send("Error retrieving users: " + err);
+    }
+    res.render("users.ejs", { users: users });
+  });
+});
+
+// optional dashboard with both
+app.get("/dashboard", (req, res) => {
+  connection.query("SELECT * FROM users", (err, users) => {
+    if (err) {
+      return res.status(500).send("Error retrieving users: " + err);
+    }
+    connection.query("SELECT * FROM posts", (err, posts) => {
+      if (err) {
+        return res.status(500).send("Error retrieving posts: " + err);
+      }
+      res.render("dashboard.ejs", { users: users, posts: posts });
+    });
+  });
+});
+
 // 404
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
 });
-// Start the app
+
+// start the app
 app.listen(3003, () => console.log("App running on http://127.0.0.1:3003"));
